9a92c475584b0e4b382c9bf22103e863f6ad19fb
Jeremy Marquis <jeremy@jeremymarquis.com>
2016-09-08 22:00:34 -0400

    temporary removal of filtering

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def lists
-    render json: Project.find(params[:projectId]).lists, include: { cards: { only: :name }}
+    render json: Project.find(params[:projectId]).lists, include: :cards
   end
 
 end

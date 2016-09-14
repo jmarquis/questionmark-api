11aed1103828b8d894fc9df5dcd2d68d103c2feb
Jeremy Marquis <jeremy@jeremymarquis.com>
2016-09-13 22:40:53 -0400

    enable cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
 
-  skip_before_filter :authenticate
+  skip_before_action :authenticate
 
   def create
+    puts "--------#{session[:user_id]}"
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      puts session[:user_id]
       render json: user
     else
-      render status: 400
+      render status: 403
     end
   end
 
